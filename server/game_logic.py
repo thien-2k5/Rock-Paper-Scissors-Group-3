a03b6cf686380c2599817f2ca31b55cdc7508bde
Tran Quoc Thien <thien@Trans-MacBook-Pro.local>
2026-01-19 15:47:44 +0700

    Update logic game

--- server/game_logic.py
+++ server/game_logic.py
@@ -1,37 +1,47 @@
-# Game Logic - Rock Paper Scissors
-# Thành viên 2
+# server/game_logic.py
 
-def get_result(player1_choice, player2_choice):
-    """
-    So sánh lựa chọn của 2 người chơi
-    Trả về: 'player1' nếu player1 thắng, 'player2' nếu player2 thắng, 'draw' nếu hòa
-    """
-    if player1_choice == player2_choice:
-        return "draw"
-    
-    # Player 1 thắng
-    if (
-        (player1_choice == "rock" and player2_choice == "scissors") or
-        (player1_choice == "scissors" and player2_choice == "paper") or
-        (player1_choice == "paper" and player2_choice == "rock")
-    ):
-        return "player1"
-    
-    # Player 2 thắng
-    return "player2"
+class Game:
+    def __init__(self, id):
+        self.id = id
+        self.p1Went = False
+        self.p2Went = False
+        self.moves = [None, None]
+        self.ready = False
 
+    def getPlayerMove(self, p):
+        return self.moves[p]
 
-def format_result(player1_choice, player2_choice, result):
-    """
-    Format kết quả game thành chuỗi dễ đọc
-    """
-    result_text = ""
-    
-    if result == "draw":
-        result_text = "HÒA!"
-    elif result == "player1":
-        result_text = "PLAYER 1 THẮNG!"
-    else:
-        result_text = "PLAYER 2 THẮNG!"
-    
-    return f"\n=== KẾT QUẢ ===\nPlayer 1: {player1_choice}\nPlayer 2: {player2_choice}\n{result_text}\n"
+    def play(self, player, move):
+        self.moves[player] = move
+        if player == 0:
+            self.p1Went = True
+        else:
+            self.p2Went = True
+
+    def bothGone(self):
+        return self.p1Went and self.p2Went
+
+    def resetGame(self):
+        self.p1Went = False
+        self.p2Went = False
+        self.moves = [None, None]
+
+    def findWinner(self):
+        p1 = self.moves[0]
+        p2 = self.moves[1]
+
+        if p1 == p2:
+            return -1
+
+        win_conditions = {
+            "Rock": "Scissors",
+            "Scissors": "Paper",
+            "Paper": "Rock"
+        }
+
+        if win_conditions[p1] == p2:
+            return 0
+        return 1
+
+    def isConnected(self):
+        return self.ready
